92b234ffccdcd7187a35d65f9aa8e1d3be06db78
Jonathan <poisson.jonathan777@gmail.com>
2016-09-06 22:15:16 -0400

    Simple array example

---
+++ array.py
@@ -0,0 +1,23 @@
+nums = [1,2,3,4,5]
+
+print(nums[0])
+
+print(nums[1:3])
+
+for i in range(len(nums)):
+    if nums[i] == 4:
+        print("We have found 4")
+        break
+
+# for i in nums:
+#     if i == 4:
+#         print("We have found 4")
+#         break
+
+nums.insert(0,10)
+
+print(nums)
+
+nums[0] = 100
+
+print(nums)

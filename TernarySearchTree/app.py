883d6f70661c3d22fbf2ca6ddac5747b09f68e19
Jonathan <poisson.jonathan777@gmail.com>
2016-10-01 20:39:18 -0400

    Tested getNumberOfSimilar in TST

--- TernarySearchTree/app.py
+++ TernarySearchTree/app.py
@@ -3,7 +3,11 @@ from TST import TST
 tree = TST()
 
 tree.put('apple', 100)
+tree.put('app', 500)
 tree.put('orange', 200)
 
 print(tree.get('apple'))
+print(tree.get('app'))
 print(tree.get('orange'))
+
+print tree.getNumberOfSimilar('app')

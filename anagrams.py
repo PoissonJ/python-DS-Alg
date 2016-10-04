a9fe0e7daefed0f5122870540f0622b5d1c89575
Jonathan Poisson <jonathan.poisson@summitholdings.com>
2016-10-04 11:06:04 -0400

    Added counter

--- anagrams.py
+++ anagrams.py
@@ -1,11 +1,15 @@
 def permutations(string, step = 0):
+  global counter
   if step == len(string):
     print "".join(string)
+    counter += 1
 
   for i in range(step, len(string)):
     str_copy = list(string)
     str_copy[step], str_copy[i] = str_copy[i], str_copy[step]
     permutations(str_copy, step + 1)
 
-str = 'abc'
+str = ':)'
+counter = 0
 permutations(str)
+print counter

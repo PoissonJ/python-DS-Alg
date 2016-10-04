42cb89e15789f0cb1b7bf928e72e2ddd3c288ff1
Jonathan Poisson <jonathan.poisson@summitholdings.com>
2016-10-04 11:09:42 -0400

    Refactored counter to class

--- anagrams.py
+++ anagrams.py
@@ -1,15 +1,25 @@
+class Counter(object):
+  def __init__(self):
+    self.count = 0
+  def update(self):
+    self.count += 1
+  def getCount(self):
+    return self.count
+
+
 def permutations(string, step = 0):
-  global counter
   if step == len(string):
     print "".join(string)
-    counter += 1
+    count_obj.update()
 
   for i in range(step, len(string)):
     str_copy = list(string)
     str_copy[step], str_copy[i] = str_copy[i], str_copy[step]
     permutations(str_copy, step + 1)
 
+count_obj = Counter()
+
 str = ':)'
-counter = 0
 permutations(str)
-print counter
+
+print count_obj.getCount()
